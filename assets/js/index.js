b897ef3f178f4f437490c948f53c26317341a6af
adreansyahaswafi <adreansyah.aswafi@sg-dsa.com>
2023-02-22 19:10:22 +0700

    fixing bootstraping

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+// let judul = document.getElementById('text-mobil');
+// let typoGraphy = document.createElement('h1');
+// typoGraphy.className = "title-h1"
+// typoGraphy.textContent = 'Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)';
+// const clas = judul.prepend(typoGraphy)
+// console.log();
+// const dom = document.getElementById('test')
+// console.log(dom);
+// dom.removeAttribute('class')
+// const input = document.getElementById('input')
+
+// dom.addEventListener('click', (e) => {
+//     console.log("x");
+// })
+
+// input.addEventListener('change', (event) => {
+//     console.log(event);
+// })
+
+
+
+
+// for (let i = 0; i < dom.length; i++) {
+//     setInterval(() => {
+//         dom[i].style.padding = "20px"
+//         dom[i].style.background = "yellow"
+//         dom[i].style.color = "black"
+//         setTimeout(() => {
+//             dom[i].style.padding = "30px"
+//             dom[i].style.background = "red"
+//             dom[i].style.color = "white"
+//         }, 500)
+//     }, 1000)
+// }
